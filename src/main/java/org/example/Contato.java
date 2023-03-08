dbd937f974a934ec9cf8b1185e189785b74fff08
leofab <leofabr3@gmail.com>
2023-03-08 11:47:56 -0300

    cami-la streamapi refactoring pull github

---
+++ src/main/java/org/example/Contato.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Contato {
+    String nome;
+    Integer numero;
+
+    public Contato() {
+    }
+
+    public Contato(String nome, Integer numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return nome.equals(contato.nome) && numero.equals(contato.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numero);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", numero=" + numero +
+                '}';
+    }
+
+
+}
